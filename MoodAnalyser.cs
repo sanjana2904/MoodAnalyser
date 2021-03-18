0793d5a2a159e7feed648f374b101b39dd436fab
sanjana k <sanjanarthi@gmail.com>
2021-03-18 16:51:16 +0530

    Add project files.

---
+++ MoodAnalyser.cs
@@ -0,0 +1,36 @@
+﻿using MoodAnalyser;
+using System;
+
+namespace MoodAnalyser1
+{
+    class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMoodLive1()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
+
